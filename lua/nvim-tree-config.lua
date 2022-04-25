67305771b4609ef2380fc100daa22576db1bbc67
Hayden G <hhgarland@protonmail.com>
2022-04-25 05:50:18 -0500

    updated settings for nvim tree

--- lua/nvim-tree-config.lua
+++ lua/nvim-tree-config.lua
@@ -8,10 +8,10 @@ require'nvim-tree'.setup {
   open_on_tab         = false,
   hijack_cursor       = false,
   update_cwd          = false,
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
+  -- update_to_buf_dir   = {
+  --   enable = true,
+  --   auto_open = true,
+  -- },
   diagnostics = {
     enable = true,
     icons = {
@@ -44,7 +44,7 @@ require'nvim-tree'.setup {
     height = 30,
     hide_root_folder = false,
     side = 'left',
-    auto_resize = true,
+    -- auto_resize = true,
     mappings = {
       custom_only = false,
       list = {}
